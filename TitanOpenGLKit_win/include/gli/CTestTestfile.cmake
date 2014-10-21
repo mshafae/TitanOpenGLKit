62af969d05f77ef17a439fdedeb778e983000709
Michael Shafae <mshafae@fullerton.edu>
2014-10-21 15:06:13 -0700

    Initial import of old OpenGL kit project that was not versioned but stored on kamenka.ecs.fullerton.edu

---
+++ TitanOpenGLKit_win/include/gli/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/cygwin64/home/mshafae/tmpspace/openglkit/win_build/gli-0.5.1.0/gli
+# Build directory: C:/cygwin64/home/mshafae/tmpspace/openglkit/win_build/gli-0.5.1.0/gli
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
