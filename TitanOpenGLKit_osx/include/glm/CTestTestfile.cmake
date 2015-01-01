d1cbdd3fa4d4ce2ee3eb4cbd8b2fd87ce7c9dc5d
Michael Shafae <mshafae@fullerton.edu>
2015-01-01 00:32:42 -0500

    Built all the Apple Stuff

---
+++ TitanOpenGLKit_osx/include/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mshafae/github/TitanOpenGLKit/mac_build/glm/glm
+# Build directory: /Users/mshafae/github/TitanOpenGLKit/mac_build/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
